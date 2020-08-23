16aeefcae88ee1b8988525092e3529e0a1001947
rfrancomedicosoft <69821255+rfrancomedicosoft@users.noreply.github.com>
2020-08-22 23:39:23 -0500

    Update find_pair.py

--- find_pair.py
+++ find_pair.py
@@ -4,17 +4,17 @@
 
 
 def find_pair(array, sum):
-    for i in range (0, len(array), 1):
+    for i in range (0, len(array), 1): #iteración para leer todos los datos del array.
         z = array [i]
-        for j in range (1, len(array)-1, 1):
+        for j in range (i+1, len(array), 1): #iteración para leer todos los datos del array una posición adelante del anterior 
             y = array [j]
             if z + y == sum:
                 print (z, y)
-        array [i] = 0   
+       # array [i] = 0   #Esta fue la solución para la duplicación de pares. Alcance a corregirlo.
             
             
 
-def envio_variables():
+def envio_variables():     #Función para enviar los datos.
     array = [2,4,0,6,1,7,8,3,2,5]
     sum = 10
     find_pair(array, sum)
